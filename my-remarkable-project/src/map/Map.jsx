7bb2d9679471fde7d2332119467e0abab8e3c184
elchriso75 <chris.spence@outlook.com>
2023-06-07 13:50:52 +1200

    Within the react component, obtain a reference to the div element containing the map image.
    This is required for the next step where we will use this to calculate the bounding rectangle of the map which we can then use to scale the x/y based on the map size.
    
    The following link provides the explanation for what we did in this commit - https://react.dev/learn/manipulating-the-dom-with-refs

--- my-remarkable-project/src/map/Map.jsx
+++ my-remarkable-project/src/map/Map.jsx
@@ -1,17 +1,21 @@
 import { Header } from '../components/Header';
+import { useRef } from 'react';
 
 export { Map }
 
 function Map() {
+    const mapRef = useRef(null)
 
     function handleMapClick(e) {
-        alert(`x: ${e.clientX} y: ${e.clientY}`)
+        alert(mapRef.current.innerHTML)
+
+        
     }
 
     return (
         <div className="App map">
             <Header />
-            <div className="remarkable-content">
+            <div className="remarkable-content" ref={mapRef}>
                 <div onClick={handleMapClick}>
                     <img src="nzski-the-remarkables-map-2021-web-1600x900-map-only.jpg" alt="Terrain Map" width="100%"/>
                 </div>
