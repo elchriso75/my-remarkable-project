a2ac2ab76410070582ed2ca006fee5a955c7b64b
elchriso75 <chris.spence@outlook.com>
2023-06-07 13:43:44 +1200

    1. removed bottom margin from header 2. added ski terrain map to Map page

--- my-remarkable-project/src/map/Map.jsx
+++ my-remarkable-project/src/map/Map.jsx
@@ -8,7 +8,9 @@ function Map() {
         <div className="App map">
             <Header />
             <div className="remarkable-content">
-                The map page
+                <div>
+                    <img src="nzski-the-remarkables-map-2021-web-1600x900-map-only.jpg" alt="Terrain Map" width="100%"/>
+                </div>
             </div>
         </div>
     );
