c110ccba4d1e5e12d33149a668eb3c8ef841ab96
elchriso75 <chris.spence@outlook.com>
2023-06-07 13:44:25 +1200

    Capture x/y when clicking on the map

--- my-remarkable-project/src/map/Map.jsx
+++ my-remarkable-project/src/map/Map.jsx
@@ -4,11 +4,15 @@ export { Map }
 
 function Map() {
 
+    function handleMapClick(e) {
+        alert(`x: ${e.clientX} y: ${e.clientY}`)
+    }
+
     return (
         <div className="App map">
             <Header />
             <div className="remarkable-content">
-                <div>
+                <div onClick={handleMapClick}>
                     <img src="nzski-the-remarkables-map-2021-web-1600x900-map-only.jpg" alt="Terrain Map" width="100%"/>
                 </div>
             </div>
